963d9389c54f42f609dfca88c53357756ff3b816
Chinmay Nair <zardlord08@gmail.com>
2025-05-22 19:41:17 -0400

    updated .gradle files and added ArmIO.java file

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "SingleJointedArm";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "ee6620e92056fe86f9dccd75ada0cc7f67e100a6";
+  public static final String GIT_DATE = "2025-05-22 19:12:35 EDT";
+  public static final String GIT_BRANCH = "subsystem";
+  public static final String BUILD_DATE = "2025-05-22 19:30:17 EDT";
+  public static final long BUILD_UNIX_TIME = 1747956617798L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
